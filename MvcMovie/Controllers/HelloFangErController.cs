07a18d738902a6f964c085b1ab49a38f9b121b59
FreyLee <adwsyx@163.com>
2017-03-21 15:19:08 +0800

    添加项目文件。

---
+++ MvcMovie/Controllers/HelloFangErController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloFangErController : Controller
+    {
+        // GET: HelloFangEr
+        public ActionResult Index()
+        {
+            ViewBag.msg = "Hello! 方二";
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "项目描述";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "联系作者";
+
+            return View();
+        }
+    }
+}
